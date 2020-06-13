cd3f1fe531607dbbd186f663642b6e9319247e35
jgarcia102066 <jgarcia102066@gmail.com>
2020-06-13 17:51:45 -0500

    Department and User Updates
    Added initialize to departments and users routes.

--- backend/routes/departments.js
+++ backend/routes/departments.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 let Department = require('../models/department.model');
 
+let message = "";
+
 router.route('/').get((req, res) => {
     //returns only active departments
     Department.find({departmentactive: true})
@@ -78,5 +80,46 @@ router.route('/delete/:id').delete((req, res) => {
     })
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/deleteall').delete((req, res) =>{
+    //deletes all records from collection
+    deleteAll();
+});
+
+router.route('/initialize').post((req, res) => {
+    
+    deleteAll();
+    if (res.headersSent) return;
+    addDepartment(res, '000010', 'Account Service', true);
+    if (res.headersSent) return;
+    addDepartment(res, '000020', 'Accounting', true);
+    if (res.headersSent) return;
+    addDepartment(res, '000030', 'HR', true);
+    if (res.headersSent) return;
+    addDepartment(res, '000040', 'IT', true);
+    if (res.headersSent) return;
+    addDepartment(res, '000050', 'Media', true);
+    if (res.headersSent) return;
+    addDepartment(res, '000060', 'Studio Services', true);
+    if (res.headersSent) return;
+    res.json("Data Initialized!");
+});
+
+function addDepartment(res, departmentcode, departmentname, departmentactive){
+    const newDepartment = new Department({
+        departmentcode, 
+        departmentname, 
+        departmentactive
+    });
+    newDepartment.save()
+    .then()
+    .catch(err => res.status(400).json('Error: ' + err));
+}
+
+function deleteAll(res){
+    Department.deleteMany({})
+    .then()
+    .catch(err => res.status(400).json('Error: ' + err));
+}
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,20 +7,127 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/all').post((req, res) => {
+    User.find({useractive: true})
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const usercode = req.body.usercode;
     const departmentcode = req.body.departmentcode;
     const useractive = Boolean(req.body.useractive);
 
-    let newUser = new User({usercode, username, departmentcode, useractive});
+    User.find({usercode: req.body.usercode})
+    .then(user => {
 
-    newUser.save()
-    .then(() => res.json('User Added'))
+        if (user.length){
+            user.username = username;
+            user.departmentcode = departmentcode;
+            user.useractive = useractive;
+
+            user.save()
+            .then(() => res.json('Existing User Activated!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        } else {
+            const newUser = new User({
+                usercode, 
+                username, 
+                departmentcode,
+                useractive
+            });
+        
+            newUser.save()
+            .then(() => res.json('User Added'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        }
+    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
+router.route('/update/:id').post((req, res) => {
+    User.findById(req.params.id)
+    .then(user => 
+        {
+            user.usercode = req.body.usercode;
+            user.username = req.body.username;
+            user.departmentcode = req.body.departmentcode;
+            user.useractive = Boolean(req.body.useractive);
+
+            user.save()
+            .then(() => res.json('User Updated!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/delete/:id').delete((req, res) => {
+    //not a true delete, only making active status false
+    User.findById(req.param.id)
+    .then(user => {
+        user.departmentactive = false;
+
+        user.save()
+        .then(() => res.json('User Deleted!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/deleteall').delete((req, res) =>{
+    //deletes all records from collection
+    deleteAll();
+});
+
+router.route('/initialize').post((req, res) => {
+    
+    deleteAll();
+    if (res.headersSent) return;
+    addUser(res, '000000001','Michelle', '000010', true);
+    //addUser(res, 'GSD10', 'Account Service', true);
+    if (res.headersSent) return;
+    addUser(res, '000000002','Marty', '000020', true);
+    //addUser(res, 'GSD20', 'Accounting', true);
+    if (res.headersSent) return;
+    addUser(res, '000000003','Marci', '000030', true);
+    //addUser(res, 'GSD30', 'HR', true);
+    if (res.headersSent) return;
+    addUser(res, '000000004','Loretta', '000040', true);
+    //addUser(res, 'GSD40', 'IT', true);
+    if (res.headersSent) return;
+    addUser(res, '000000005','Yolanda', '000050', true);
+    //addUser(res, 'GSD50', 'Media', true);
+    if (res.headersSent) return;
+    addUser(res, '000000006','Anne', '000060', true);
+    //addUser(res, 'GSD60', 'Studio Services', true);
+    if (res.headersSent) return;
+    res.json("Data Initialized!");
+});
+
+function addUser(res, usercode, username, departmentcode, useractive){
+    const newUser = new User({
+        usercode, 
+        username,
+        departmentcode, 
+        useractive
+    });
+    newUser.save()
+    .then()
+    .catch(err => res.status(400).json('Error: ' + err));
+}
+
+function deleteAll(res){
+    User.deleteMany({})
+    .then()
+    .catch(err => res.status(400).json('Error: ' + err));
+}
 
 
 module.exports = router;
